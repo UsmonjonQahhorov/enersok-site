8b417cd1314a6bb91d1aecf5514a14d3be172329
SmokeeMoo <smokeemoo@yandex.ru>
2024-10-22 20:32:10 +0500

    button component fix

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -4,11 +4,11 @@ import * as React from 'react';
 import { cn } from '@/utils/cn';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+	'inline-flex items-center justify-center whitespace-nowrap rounded-[100px] text-lg font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	{
 		variants: {
 			variant: {
-				default: 'bg-primary text-white hover:bg-primary/90',
+				default: 'bg-button1 text-white hover:bg-button1/90',
 				destructive: 'bg-destructive text-destructive hover:bg-destructive/90',
 				outline:
 					'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
@@ -17,9 +17,9 @@ const buttonVariants = cva(
 				link: 'text-white underline-offset-4 hover:underline',
 			},
 			size: {
-				default: 'h-10 px-4 py-2',
-				sm: 'h-9 rounded-md px-3',
-				lg: 'h-11 rounded-md px-8',
+				default: 'px-10 py-5 leading-[normal]',
+				sm: 'px-4 py-5 leading-[normal]',
+				lg: 'px-6 py-5 leading-[normal]',
 				icon: 'h-10 w-10',
 			},
 		},
