04c1dade1c26d3a02b38f5702c0df0d27cfbf2d7
SmokeeMoo <smokeemoo@yandex.ru>
2024-10-24 01:26:39 +0500

    grmsubmissionform component create

---
+++ frontend/src/components/form/GrmSubmissionForm/GrmSubmissionForm.tsx
@@ -0,0 +1,54 @@
+import type { FC } from 'react';
+import { TextInput } from '@/components/input/TextInput/TextInput';
+import { InputBase } from '@/components/input/InputBase';
+import { Textarea } from '@/components/input/TextArea';
+import { Button } from '@/components/ui/Button';
+import { cn } from '@/utils/cn';
+
+export const GrmSubmissionForm: FC<GrmSubmissionFormProps> = ({
+    email,
+    message,
+    name,
+    phone,
+    sumbmit,
+    className
+}) => {
+    return (
+        <form className={cn(className, 'p-12 bg-white flex flex-col gap-y-10 rounded-xl')}>
+            <TextInput
+                placeholder={name}
+                required={true}
+            />
+            <InputBase
+                type='email'
+                placeholder={email}
+                required={true}
+            />
+            <InputBase
+                type='tel'
+                placeholder={phone}
+                required={true}
+                pattern="[+]{1}[0-9]{7,15}"
+            />
+            <Textarea
+                placeholder={message}
+                required={true}
+            />
+            <Button
+                type='submit'
+                className='w-fit'
+            >
+                {sumbmit}
+            </Button>
+        </form>
+    )
+}
+
+interface GrmSubmissionFormProps {
+    className?: string;
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
+    sumbmit: string;
+}
