b64f6f65240f298b3459925d67c72727347fbca0
SmokeeMoo <smokeemoo@yandex.ru>
2024-10-24 22:06:55 +0500

    newcard component create

---
+++ frontend/src/components/ui/NewCard/NewCard.tsx
@@ -0,0 +1,46 @@
+import { cn } from '@/utils/cn';
+import Image, { type StaticImageData } from 'next/image';
+import type { FC } from 'react';
+import { Heading } from '../Heading';
+import { Paragraph } from '../Paragraph';
+import Time from './../../../../public/time.svg';
+
+export const NewCard: FC<NewCardProps> = ({
+    date,
+    time,
+    title,
+    className,
+    image
+}) => {
+    return (
+        <div className={cn(className, 'flex flex-col gap-y-8 items-start text-secondary')}>
+            <div className='w-full h-full'>
+                <Image
+                    src={image}
+                    alt='Enersok News Image'
+                    className='max-h-[287px] w-full h-full object-cover object-center rounded-xl'
+                />
+            </div>
+            <Heading className='text-[32px]'>{title}</Heading>
+            <div className='flex flex-row gap-x-5 items-center'>
+                <Paragraph className='text-base !leading-[normal]'>{date}</Paragraph>
+                <div className='flex flex-row items-center gap-x-1'>
+                    <Image
+                        src={Time}
+                        alt='Time Enersok News'
+                        className='w-4 h-4'
+                    />
+                    <Paragraph className='!leading-[normal]'>{time}</Paragraph>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+interface NewCardProps {
+    className?: string;
+    title: string;
+    date: string;
+    time: string;
+    image: StaticImageData;
+}
