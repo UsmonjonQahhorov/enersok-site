4994fc715b06871b95524f8b249ec8efde245fa8
SmokeeMoo <smokeemoo@yandex.ru>
2024-11-06 21:08:14 +0500

    mobileNavigation create

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import type { FC } from 'react';
 import Logo from '@public/logo.png';
 import { Container } from '@/components/ui/Container';
-import { DesktopNavigation } from './components';
+import { DesktopNavigation, MobileNavigation } from './components';
 import { cn } from '@/utils/cn';
 import { Link } from '@/i18n/routing';
 
 export const Header: FC<HeaderProps> = ({ className }) => {
 	return (
 		<header
-			className={cn(className, 'py-12 absolute top-0 left-0 w-full z-[100]')}
+			className={cn(className, 'py-12 absolute top-0 left-0 w-full z-[40]')}
 		>
 			<Container className="flex justify-between items-center">
 				<Link href={'/'}>
@@ -21,6 +21,7 @@ export const Header: FC<HeaderProps> = ({ className }) => {
 					/>
 				</Link>
 				<DesktopNavigation />
+				<MobileNavigation  />
 			</Container>
 		</header>
 	);
--- frontend/src/locales/navigations.ts
+++ frontend/src/locales/navigations.ts
@@ -49,3 +49,46 @@ export const navigation = [
 		href: RouterConfig.GRMSubmission,
 	},
 ];
+
+export const mobileNavigation = [
+	{
+		name_en: 'About Company',
+		name_uz: 'Kompaniya haqida',
+		href: RouterConfig.AboutCompany,
+	},
+	{
+		name_en: 'Organizational Structure',
+		name_uz: 'Tashkiliy tuzilma',
+		href: RouterConfig.OrganizationalStructure,
+	},
+	{
+		name_en: 'About Sponsors',
+		name_uz: 'Sponsorlar haqida',
+		href: RouterConfig.AboutSponsors,
+	},
+	{
+		name_en: 'Documents and Guidelines',
+		name_uz: 'Hujjatlar va qo`llanmalar',
+		href: RouterConfig.DocumentsAndGuidelines,
+	},
+	{
+		name_en: 'Project Details',
+		name_uz: 'Loyiha Tafsilotlari',
+		href: RouterConfig.ProjectDetails,
+	},
+	{
+		name_en: 'News',
+		name_uz: 'Yangiliklar',
+		href: RouterConfig.News,
+	},
+	{
+		name_en: 'Careers',
+		name_uz: 'Karyera',
+		href: RouterConfig.Careers,
+	},
+	{
+		name_en: 'GRM Submission',
+		name_uz: 'GRM topshirish',
+		href: RouterConfig.GRMSubmission,
+	},
+]
