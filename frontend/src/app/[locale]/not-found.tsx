d3ef7de2ad1aa267d42fee6081991ce976fe6495
yusuf-saydullayev <yusufsaydullayev@gmail.com>
2024-12-01 20:38:18 +0500

    update 404 page with custom image

--- frontend/src/app/[locale]/not-found.tsx
+++ frontend/src/app/[locale]/not-found.tsx
@@ -1,12 +1,15 @@
-import { Heading } from '@/components/ui/Heading';
+import Picture404 from '@public/404.webp';
+import NextImage from 'next/image';
 
-// TODO: Better 404 page
 export default function NotFound() {
 	return (
-		<div className="h-[100vh] text-center">
-			<Heading className="text-red-600 text-center my-auto">
-				LOCALE NOT FOUND
-			</Heading>
-		</div>
+		<section className="text-center relative">
+			<NextImage
+				src={Picture404}
+				alt="404"
+				className="mx-auto w-full h-full"
+				quality={100}
+			/>
+		</section>
 	);
 }
