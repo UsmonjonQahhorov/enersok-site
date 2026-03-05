a4deb372c0dd87f7d243b8fc9be4f2b1b5f3f36e
Hello Sir! <qohhorovusmonjon@gmail.com>
2026-03-05 19:31:20 +0500

    initial commit

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,65 +1,50 @@
 export default () => ({
-     "fuzzy-search": {
-          enabled: true,
-          config: {
-               contentTypes: [
-                    {
-                         uid: 'api::news.news',
-                         modelName: 'new',
-                         transliterate: true,
-                         fuzzysortOptions: {
-                              characterLimit: 300,
-                              threshold: 0.6,
-                              limit: 10,
-                              keys: [
-                                   {
-                                        name: "preview_title",
-                                        weight: 0.1,
-                                   },
-                                   {
-                                        name: "news_description",
-                                        weight: -0.1,
-                                   },
-                                   {
-                                        name: "news_description_full",
-                                        weight: -0.1,
-                                   },
-                              ],
-                         },
-                    },
-                    {
-                         uid: 'api::vacancy.vacancy',
-                         modelName: 'vacancies',
-                         transliterate: true,
-                         fuzzysortOptions: {
-                              characterLimit: 300,
-                              threshold: 0.6,
-                              limit: 10,
-                              keys: [
-                                   {
-                                        name: "vacancy_name",
-                                        weight: 0.2,
-                                   },
-                              ],
-                         },
-                    },
-                    {
-                         uid: 'api::documents-and-guidline.documents-and-guidline',
-                         modelName: 'vacancies',
-                         transliterate: true,
-                         fuzzysortOptions: {
-                              characterLimit: 300,
-                              limit: 10,
-                              threshold: 0.6,
-                              keys: [
-                                   {
-                                        name: "document_name",
-                                        weight: 0.3,
-                                   },
-                              ],
-                         },
-                    },
-               ]
-          }
-     },
+  upload: {
+    enabled: false,
+  },
+
+  "fuzzy-search": {
+    enabled: true,
+    config: {
+      contentTypes: [
+        {
+          uid: "api::news.news",
+          modelName: "new",
+          transliterate: true,
+          fuzzysortOptions: {
+            characterLimit: 300,
+            threshold: 0.6,
+            limit: 10,
+            keys: [
+              { name: "preview_title", weight: 0.1 },
+              { name: "news_description", weight: -0.1 },
+              { name: "news_description_full", weight: -0.1 },
+            ],
+          },
+        },
+        {
+          uid: "api::vacancy.vacancy",
+          modelName: "vacancies",
+          transliterate: true,
+          fuzzysortOptions: {
+            characterLimit: 300,
+            threshold: 0.6,
+            limit: 10,
+            keys: [{ name: "vacancy_name", weight: 0.2 }],
+          },
+        },
+        {
+          uid: "api::documents-and-guidline.documents-and-guidline",
+          modelName: "vacancies",
+          transliterate: true,
+          fuzzysortOptions: {
+            characterLimit: 300,
+            limit: 10,
+            threshold: 0.6,
+            keys: [{ name: "document_name", weight: 0.3 }],
+          },
+        },
+      ],
+    },
+  },
 });
